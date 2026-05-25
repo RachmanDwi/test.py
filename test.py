413bd77feeec31c9abbee1ea6b9fd55e03d08ace
RachmanDwi <wamentimen@gmail.com>
2026-05-25 21:35:56 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
-# from selenium import webdriver
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
+# Baca Excel
+file_excel = r"C:\Users\ratna\OneDrive\Desktop\template_entry_kunjungan.xlsx"
+data = pd.read_excel(file_excel)
+
+# Ambil pasien pertama
+pasien = data.iloc[0]
+
+# Buka Chrome
 driver = webdriver.Chrome()
 
-driver.get("https://google.com")
+# Buka website BPJS
+driver.get("https://pcarejkn.bpjs-kesehatan.go.id/eclaim/")
 
-print("Chrome berhasil dibuka")
+print("Silakan login dulu...")
+time.sleep(20)
 
-time.sleep(5)
+print("Nama pasien dari Excel:")
+print(pasien["Nama Pasien"])
+
+# Chrome tetap terbuka
+input("Tekan ENTER untuk menutup browser...")
 
 driver.quit()
